a72099f0fd1f02df28733bf9ae15969a5a48facb
Git-xht <1443921468@qq.com>
2020-06-21 15:05:57 +0800

    代码

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,29 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class Fibonacci {
-	private static double[] fib;
+	private static List<Integer> fib = new ArrayList<Integer>(1);
 	public static int N;
 	
 	public static void buildFibonacci(int N) {
-		Fibonacci.N = N;
-		Fibonacci.fib = new double[N + 1];
-		if(Fibonacci.N >= 2) {
-			Fibonacci.fib[1] = 1;
-			Fibonacci.fib[2] = 1;
-			for(int i = 3;i <= N;++i)
-				Fibonacci.fib[i]= Fibonacci.fib[i - 1] + Fibonacci.fib[i - 2]; 
-		}
-		else if (Fibonacci.N >= 1) {
-			Fibonacci.fib[1] = 1;
+		if(N < 1) return;
+		fib.add(0);
+		fib.add(1);
+		fib.add(1);
+		for(int i = 3;fib.get(i-1) + fib.get(i-2) < N;++i) {
+			fib.add(fib.get(i-1) + fib.get(i-2));
 		}
 	}
 	
-	public static double of(int idx) {
-		return fib[idx];
+	public static int of(int idx) {
+		return fib.get(idx);
 	}
 
 	public static void main(String[] args) {
 		Fibonacci.buildFibonacci(200);
-		for(int i = 1;i <= Fibonacci.N;++i)
+		for(int i = 1;i < Fibonacci.fib.size();++i)
 			System.out.println("Fibonacci.of(" + i + ") = " + Fibonacci.of(i));
 
 	}
